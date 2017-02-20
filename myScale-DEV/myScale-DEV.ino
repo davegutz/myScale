a31de46be76397e716e870de6630a72116333cd0
Dave G <davegutz@alum.mit.edu>
2017-02-20 17:31:12 -0500

    debug

--- myScale-DEV/myScale-DEV.ino
+++ myScale-DEV/myScale-DEV.ino
@@ -11,13 +11,28 @@
 #include "application.h"
 #include "Hx711.h"
 Hx711 scale(A2, A3);
+#define BUTTON_PIN D2                                          // Button 3-way input momentary 3.3V, steady GND (D2)
 
 void setup() {
   Serial.begin(9600);
+  pinMode(BUTTON_PIN, INPUT);
 }
 
 void loop() {
-  Serial.print(scale.getGram(), 1);
-  Serial.println(" g");
-  delay(200);
+  int buttonState = 0;                    // Pushbutton
+  unsigned long now = millis();           // Keep track of time
+  static unsigned long lastButton = 0UL;  // Last button push time, millis
+  static double        gramNull   = 0;    // Tare
+  double gram   = scale.getGram();
+  double gramNet = gram - gramNull;
+  double oz = gramNet/454*16;
+  Serial.printf(" %4.1fg, %4.2foz\n", gramNet, oz);
+  buttonState = digitalRead(BUTTON_PIN);
+  if (buttonState==HIGH && (now - lastButton > 200UL))
+  {
+    lastButton = now;
+    gramNull = gram;
+    Serial.printf(" -------------%4.1fg\n", gramNull);
+  }
+  delay(1);
 }
