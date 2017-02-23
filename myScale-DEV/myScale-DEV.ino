2fa93952a3a4526680f438a4bebb03ee9eeb0eed
Dave G <davegutz@alum.mit.edu>
2017-02-22 19:56:17 -0500

    Add LCD

--- myScale-DEV/myScale-DEV.ino
+++ myScale-DEV/myScale-DEV.ino
@@ -4,18 +4,42 @@
  *https://github.com/aguegu/ardulibs/tree/3cdb78f3727d9682f7fd22156604fc1e4edd75d1/hx711
  */
 
-// Hx711.DOUT - pin #A2
-// Hx711.SCK - pin #A3
 
 // Standard
 #include "application.h"
 #include "Hx711.h"
+#include "LiquidCrystal_I2C.h"
+/*
+* Pin Connections:
+ * Hx711.DOUT A2
+ * Hx711.SCK  A3
+ * LCD  SCL = D1
+ *  PULL 5K = D1 to VIN
+ * LCD  SDA = D0
+ *  PULL 5K = D0 to VIN
+ * LCD  VCC = VIN
+ * LCD  GND = GND
+ * BUTT HI  = D2
+ * BUTT LO1 - 3v3
+ * BUTT LO2 - 10k to GND
+*/
+LiquidCrystal_I2C   *lcd;
 Hx711 scale(A2, A3);
 #define BUTTON_PIN D2                                          // Button 3-way input momentary 3.3V, steady GND (D2)
 
 void setup() {
+  scale.setScale(214.7);
   Serial.begin(9600);
   pinMode(BUTTON_PIN, INPUT);
+  lcd = new LiquidCrystal_I2C(0x3F, 16, 2);  // Set the LCD I2C address
+  lcd->init();                      // initialize the lcd
+  lcd->backlight();
+  lcd->clear();
+  lcd->setCursor(0,0);
+  lcd->print("              g");
+  lcd->setCursor(0,1);
+  lcd->print("              oz");
+  delay ( 1000 );
 }
 
 void loop() {
@@ -33,6 +57,22 @@ void loop() {
     lastButton = now;
     gramNull = gram;
     Serial.printf(" -------------%4.1fg\n", gramNull);
+    lcd->setCursor(0,0);
+    lcd->print("--------------");
+    lcd->setCursor(0,1);
+    lcd->print("--------------");
+    delay(400);
   }
+
+  // Display
+  lcd->setCursor(0,0);
+  lcd->print("              g");
+  lcd->setCursor(0,1);
+  lcd->print("              oz");
+  lcd->setCursor(8,0);
+  lcd->print(gramNet);
+  lcd->setCursor(8,1);
+  lcd->print(oz);
+
   delay(1);
 }
